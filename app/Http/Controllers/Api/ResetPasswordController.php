db721e3b662746bf9bf409fd89e3f1c781812d2f
Your Name <onwabrains33@gmail.com>
2022-08-16 10:58:38 +0100

    Testing password reset

--- app/Http/Controllers/Api/ResetPasswordController.php
+++ app/Http/Controllers/Api/ResetPasswordController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-use App\Models\{User, Password};
+use App\Models\User Password};
 use App\Http\Controllers\Controller;
 use App\Mail\PasswordReset;
 use Illuminate\Support\Str;
